5c7973b96d6cfaaa236729303a3ac05a2af508e4
Terelld <terelld@gmail.com>
2023-09-03 16:18:36 -0500

    update new order to new note and all related routing

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import NewNotePage from '../NewNotePage/NewNotePage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import { getUser } from '../../utilities/users-service';
 
@@ -17,7 +17,7 @@ export default function App() {
         <>
           <NavBar user={ user }  setUser={ setUser }/>
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders/new" element={<NewNotePage />} />
             <Route path="/orders" element={<OrderHistoryPage />} />
           </Routes>
         </>
